aaf5fab8943ac4e8bec6d5d8458e0e44fdc74d87
Kareem ElZeky <kareem.elzeky@gmail.com>
2023-04-08 13:06:10 +0200

    Fixed import bug

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user/user.controller';
-import { ServicesService } from './services/services.service';
-import { UserService } from './services/user/user.service';
-import { AuthService } from './services/auth/auth.service';
 
 @Module({
   controllers: [UserController],
-  providers: [ServicesService, UserService, AuthService],
+  providers: [],
 })
 export class UserModule {}
