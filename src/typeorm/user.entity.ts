eb2c745462b1ffc6178b240016237ae9f36260e0
Kareem ElZeky <kareem.elzeky@gmail.com>
2023-04-08 14:43:03 +0200

    Edited properties' types

--- src/typeorm/user.entity.ts
+++ src/typeorm/user.entity.ts
@@ -2,7 +2,6 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 @Entity()
 export class User {
-
   @ApiProperty({
     example: 0,
     description: 'The id of the User',
@@ -17,11 +16,14 @@ export class User {
     example: 'string',
     description: 'The name of the User',
   })
-  
   @Column({
     name: 'name',
     nullable: false,
     default: '',
+    // This corresponds to varchar(255) in Postgres
+    type: 'varchar',
+    length: 255,
+
   })
   name: string;
 
@@ -34,14 +36,16 @@ export class User {
     nullable: false,
     default: '',
     unique: true,
+    // This corresponds to varchar(255) in Postgres
+    type: 'varchar',
+    length: 255,
   })
   email: string;
-
 
   @Column({
     nullable: false,
     default: '',
+    type: 'varchar',
   })
   password: string;
-
 }
