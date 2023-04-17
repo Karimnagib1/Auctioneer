be702c96c5e2177685446c9ea9a25858d8f6b8d9
Kareem ElZeky <kareem.elzeky@gmail.com>
2023-04-17 17:37:11 +0200

    Created auctions service with basic functionality

--- src/auctions/auctions.module.ts
+++ src/auctions/auctions.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuctionsController } from './controllers/auctions/auctions.controller';
 import { AuctionsService } from './services/auctions/auctions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Auction, AuctionToUser, User } from 'src/typeorm';
 @Module({
   controllers: [AuctionsController],
   providers: [AuctionsService],
+  imports: [TypeOrmModule.forFeature([User, Auction, AuctionToUser])],
 })
 export class AuctionsModule {}
--- src/auctions/services/auctions/auctions.service.ts
+++ src/auctions/services/auctions/auctions.service.ts
@@ -1,4 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Auction, AuctionToUser, User } from 'src/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuctionsService {}
+export class AuctionsService {
+  constructor(
+    @InjectRepository(Auction)
+    private readonly auctionRepository: Repository<Auction>,
+    @InjectRepository(AuctionToUser)
+    private readonly auctionToUserRepository: Repository<AuctionToUser>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async getAuctions() {
+    return await this.auctionRepository.find();
+  }
+
+  async getAuctionById(id: number) {
+    return await this.auctionRepository.findBy({ id: id });
+  }
+
+  async getAuctionByUserId(id: number) {
+    return await this.auctionRepository
+      .createQueryBuilder('auction')
+      .where('ownerId = :id', { id: id })
+      .getMany();
+  }
+
+  async getAuctionByWinnerId(id: number) {
+    return await this.auctionRepository
+      .createQueryBuilder('auction')
+      .where('winnerId = :id', { id: id })
+      .getMany();
+  }
+
+  async getAuctionByBidderId(id: number) {
+    return await this.auctionToUserRepository
+      .createQueryBuilder('auctionToUser')
+      .where('user_id = :id', { id: id })
+      .innerJoinAndSelect('auctionToUser.auction_id', 'auction')
+      .getMany();
+  }
+  async createAuction(ownerId: number, itemName: string, description: string) {
+    const auction = new Auction();
+    const user = await this.userRepository.findOne({ where: { id: ownerId } });
+    auction.owner = user;
+    auction.itemName = itemName;
+    auction.description = description;
+    return await this.auctionRepository.save(auction);
+  }
+  async updateAuction(
+    id: number,
+    ownerId: number,
+    itemName: string,
+    description: string,
+  ) {
+    const auction = await this.auctionRepository.findOne({ where: { id: id } });
+    const user = await this.userRepository.findOne({ where: { id: ownerId } });
+    auction.owner = user;
+    auction.itemName = itemName;
+    auction.description = description;
+    return await this.auctionRepository.save(auction);
+  }
+}
